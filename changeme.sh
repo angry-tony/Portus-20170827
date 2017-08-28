94d6358b5f4b485b3cfa390e6b444b05407b9822
HoHo Kim <hee4004@gmail.com>
2017-08-28 22:18:27 +0900

    Update changeme.sh

--- changeme.sh
+++ changeme.sh
@@ -14,3 +14,6 @@ sed -i 's/pdr-01/HOSTNAME/g' ~/Portus-20170827/config/config.yml
 
 # registry
 sed -i 's/192.0.220.105    pdr-01/192.268.0.0     hoho/g' ~/Portus-20170827/for-self-crt/registry/init
+
+# mysqldump -uroot -pportus12341234 --all-databases > /var/lib/portus/mariadb/BACKUP/backup.sql
+# mysql -uroot -pportus12341234 < backup.sql
