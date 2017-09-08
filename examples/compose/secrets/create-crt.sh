eb95f81c1bb88b1922ad8e4b536b4a3f395c091a
root <hee4004@gmail.com>
2017-09-08 18:23:12 +0900

    add crt

--- examples/compose/secrets/create-crt.sh
+++ examples/compose/secrets/create-crt.sh
@@ -2,7 +2,7 @@ openssl genrsa -out portus-CA.key 4096
 openssl req -x509 -new -nodes -key portus-CA.key -sha256 -days 3650 -out portus-CA.crt -subj '/C=XX/L=Default City/O=Default Company Ltd'
 openssl genrsa -out portus.key 4096
 
-openssl req -new -sha256 -key portus.key -out portus.csr -subj '/C=KO/ST=Gyeonggi/L=City/O=Company, INC./OU=Team/CN=pdr-01'
+openssl req -new -sha256 -key portus.key -out portus.csr -subj '/C=KO/ST=Gyeonggi/L=City/O=Company, INC./OU=Team/CN=ldap-nfs-01'
 
-echo 'subjectAltName=DNS:*.example.com,DNS:auth-portus,DNS:pdr-01,IP:192.0.220.105' > extfile.cnf
+echo 'subjectAltName=DNS:ldap-nfs-01,IP:192.168.147.14' > extfile.cnf
 openssl x509 -req -sha256 -in portus.csr -CA portus-CA.crt -CAkey portus-CA.key -CAcreateserial -out portus.crt -days 3650  -extfile extfile.cnf
